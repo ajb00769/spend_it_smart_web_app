078142f67bc814fe24545b1bc109de6a529f9dea
Allen Bercero <abercero@pop-os.localdomain>
2023-06-12 08:57:18 +0800

    added a 2nd graph

--- static/js/main_script.js
+++ static/js/main_script.js
@@ -26,3 +26,40 @@ new Chart(myChart, {
     },
   },
 });
+
+const graphIncomeData = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  data: [30000, 40000, 37000, 22000, 32000, 27000],
+};
+
+const graphExpenseData = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  data: [15000, 20000, 18000, 16000, 24000, 19000],
+};
+
+const myGraphChart = document.querySelector(".my-graph-chart");
+
+new Chart(myGraphChart, {
+  type: "line",
+  data: {
+    labels: graphIncomeData.labels,
+    graphExpenseData,
+    datasets: [
+      {
+        label: "Income",
+        data: graphIncomeData.data,
+      },
+      {
+        label: "Expenses",
+        data: graphExpenseData.data,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      legend: {
+        FontFaceSet: "Questrial",
+      },
+    },
+  },
+});
